d30bf9c73672883f0ee1fea561c2c2a819be6b9c
OlivierArgentieri <olivierargentieri@gmail.com>
2019-04-22 22:11:50 +0200

    First BUILD OK

---
+++ N3DS_BasicStarterProject/Assets/Scripts/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.N3DS;
+using Debug = UnityEngine.N3DS.Debug;
+
+public class Rotate : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(GamePad.GetButtonHold(N3dsButton.B))
+            this.transform.Rotate(Vector3.up);
+
+        if (GamePad.GetButtonHold(N3dsButton.A))
+            this.transform.Rotate(Vector3.forward);
+    }
+}
